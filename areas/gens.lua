2b45e39df80549149084326ba8a8ce2bec0f6249
k4ey <kaeyel12@gmail.com>
2024-12-14 15:27:14 +0100

    gens update!!!!

--- areas/gens.lua
+++ areas/gens.lua
@@ -1,3 +1,40 @@
+-- predefining default values
+local enableCommands, commands, commandsInterval, commandsDelay, commandsEntropy, pitch, safeMode, farmland, crops =
+    false, {}, 0, 0, 0,
+    18, false, "Farmland", {}
+--[[
+ CONFIG EXAMPLE PLEASE COPY AND PASTE WHOLE LINES! IF YOU HAVE PROBLEMS, PLEASE INPUT THIS TEXT INTO CHATGPT AND TRY FIGURING OUT WHAT IS WRONG WITH __HIM FIRST__ (ITS GONNA BE FASTER THAN ASKING ME!!!!)
+PROMPTS FOR CHATGPT:
+I WANT
+below is a example configuration of a LUA script. PROVIDE TEXT MATCHING PROMPT, DO NOT PROVIDE COMMENTS, USE VARIABLES DEFINED BY THE EXAMPLE, DO NOT INTRODUCE ANY NEW FIELDS
+
+local enableCommands = true -- true OR false TO ENABLE
+local commands = { "/command one", "/command two" } -- HAS TO START WITH "/" to be a command, enableCommands must be true for this to take effect (ITS A TABLE OF STRINGS)
+local commandsInterval = 200000 -- interval in ms (time between repeating commands)
+local commandsDelay = 500 -- delay between each command in ms (wait time between consequitive commands)
+local commandsEntropy = 100 -- random time to add  to interval (in ms)
+
+local safeMode = true -- true OR false TO ENABLE SAFE MODE (stops when any GUI is opened)
+local pitch = 18 -- pitch to look at when mining
+local crops = {
+  ["Wheat Crops"] = true,
+}
+local farmland = "Farmland"
+END OF PROMPT FOR CHATGPT
+]]
+-- >>>>PASTE YOUR CONFIG BELOW THIS LINE (WHEN PASTING MAKE SURE TO OVERWRITE LINES BELOW THIS COMMENT)<<<<
+
+
+local safeMode = true -- true OR false TO ENABLE SAFE MODE (stops when any GUI is opened)
+local crops = {
+  ["Wheat Crops"] = true,
+}
+local farmland = "Farmland"
+local pitch = 18 -- pitch to look at when farming
+
+--BUT ABOVE THIS LINE!!!
+
+crops.Air = true
 return {
   referencePoint = { 1000, 100, 1000 },
   referenceDimensions = { 10, 10, 10, },
@@ -6,20 +43,21 @@ return {
     inside = { x = true, z = true, y = true },
   },
   areas = {
-    AreaMacro.new({ 1000, 100, 1000 }, { 1010, 110, 1010 }, {
+    AreaMacro.new({ 1000, 50, 1000 }, { 1010, 141, 1010 }, {
       id = "main",
       type = "constant",
       color = "green",
       defaultCallbacksNames = {
-        "rayTraceTurning",
+        "gens",
         "goForward",
         "mine",
         "bpsCounter",
-        "perfcheck"
-
+        "perfcheck",
+        "afkbypass"
       },
       callbackArgs = {
         goForward = { sprint = true, time = 230 },
+        gens = { farmland = farmland, crops = crops, pitch = pitch },
       },
     }),
   }
---
+++ macros/gens.lua
@@ -0,0 +1,254 @@
+local vec3 = _G.libs.vec3
+local quat = _G.libs.quat
+local rb = _G.libs.relativeBlocks
+local looker = _G.libs.looker
+local function init()
+  local gens = MacroCreator:extensions("gens")
+  ---@return vec3
+  local function getPlayerPosBlockVec() return vec3(getPlayerBlockPos()) end
+  local function map(t, f)
+    local r = {}
+    local rval, rkey = nil, nil
+    for k, v in pairs(t) do
+      rval, rkey = f(v)
+      rkey = rkey or k
+      r[rkey] = rval
+    end
+    return r
+  end
+
+  local color = "red"
+  local function show(...)
+    local vs = { ... }
+    if #vs == 1 then vs = vs[1] end
+    for i, v in pairs(vs) do
+      v = v + vec3(0, 1, 0)
+      rb.sShow({ position = { v:unpack() }, color = color, opacity = 1 })
+    end
+    return ...
+  end
+  ---@type fun(x: number, y: number, z: number): string?
+  ---@diagnostic disable-next-line
+  getBlockName = getBlockName
+
+  ---@param v vec3
+  local function getCropScore(v)
+    local farmLand = getBlockName(v:unpack())
+    local crop = getBlockName((v + vec3(0, 1, 0)):unpack())
+    local headBlock = getBlockName((v + vec3(0, 2, 0)):unpack())
+    if headBlock ~= "Air" then return -999 end
+    if farmLand ~= GensConfig.farmland then return -999 end
+    if not GensConfig.crops[crop] then return 0 end
+    return crop ~= "Air" and 1 or 0
+  end
+
+  local function isCrop(v)
+    return getCropScore(v) > 0
+  end
+
+  local function isFarmLand(v)
+    return getBlockName(v:unpack()) == GensConfig.farmland
+  end
+
+  ---@class Probed: vec3
+  ---@field angle number
+
+
+  local function getAngleToBlock(v)
+    local y1, p1 = looker.getRotationTo(v)
+    local angle1 = quat.Euler(0, 0, playerDetails.getYaw())
+    local angle2 = quat.Euler(0, 0, y1)
+    local angle = angle1:Angle(angle2)
+    return angle
+  end
+
+
+
+  local function rotateTowards(v)
+    local angle = getAngleToBlock(v)
+    looker.lockYawTo(v.x, v.z,
+      math.min(math.max(playerDetails.getPitch() + math.random(-angle, angle) / 100, 15), GensConfig.pitch))
+  end
+
+
+  -- FLOOD FILL FROM PLAYER TO OBTAIN BLOCKS IN RANGE, STORE EDGES IN SET
+  local function floodFill(start, isEdge, actionCondition, action, visited)
+    ---@type table<string, vec3>
+    visited = visited or {}
+    ---@type vec3[]
+    local queue = {}
+    ---@type vec3[]
+    local edges = {}
+    actionCondition = actionCondition or function() return false end
+    action = action or function() end
+
+    ---@param v vec3
+    local function visit(v)
+      if visited[v:__tostring()] then return end
+      if isEdge(v) then
+        table.insert(edges, v)
+        return
+      end
+      visited[v:__tostring()] = v
+      if actionCondition(v) then action(v) end
+      table.insert(queue, v)
+    end
+    visit(start)
+    while #queue > 0 do
+      ---@type vec3
+      local v = table.remove(queue, 1)
+      for _, d in ipairs({ vec3(0, 0, 1), vec3(0, 0, -1), vec3(1, 0, 0), vec3(-1, 0, 0) }) do
+        local n = v + d
+        if not visited[n:__tostring()] then visit(n) end
+      end
+    end
+    return edges, visited
+  end
+
+  local function getRestrictedArea(edges)
+    ---restrict blocks closer than 5 to edge
+    ---@type table<string, vec3>
+    local visited = {}
+    rb.sShow({ clear = true })
+    for _, e in pairs(edges) do
+      floodFill(e, function(v)
+        local dist = v:distanceSquared(e)
+        return (not dist == 0 and not isCrop(v)) or dist > 25
+      end, function(v)
+        return true
+      end, function(v)
+      end, visited)
+    end
+
+    ---@type { string:  boolean }
+    local restricted = {}
+    for k, v in pairs(visited) do
+      restricted[k] = true
+    end
+    return restricted
+  end
+
+  local function degToRad(deg) return deg * math.pi / 180 end
+
+  local function getDirectionVector(block)
+    local yaw = looker.getRotationTo(block)
+    local yawRad = degToRad(yaw)
+    local v = vec3(-math.sin(yawRad), 0, math.cos(yawRad))
+    return v
+  end
+  local function getYawDirectionVector(offset)
+    local yaw = playerDetails.getYaw()
+    local yawRad = degToRad(yaw + offset)
+    local v = vec3(-math.sin(yawRad), 0, math.cos(yawRad))
+    return v
+  end
+
+  local function isSafePath(path, restricted)
+    restricted = restricted or {}
+    for _, v in pairs(path) do
+      if not isFarmLand(v) or restricted[v:__tostring()] then return false end
+    end
+    return true
+  end
+
+  local edges, inside = floodFill(getPlayerPosBlockVec(), function(v) return not isCrop(v) end)
+  local i = 0
+  inside = map(inside, function(v)
+    i = i + 1
+    return v, i
+  end)
+  local function randomGoal(pool)
+    local len = #pool
+    if len == 0 then
+      log(
+        "&4 NO FARMING AREA DETECTED!!!, GET INTO FARMING ZONE AND TRY LOADING IT AGAIN, IF THIS DOES NOT WORK, GO TO MINELESS-CONFIG-MAIN AND EDIT areas/gens.lua (more instructions there)")
+      error("", 0)
+    end
+
+    return inside[math.random(1, len)]
+  end
+
+  local restricted = getRestrictedArea(edges)
+  local function normalRoutine(goalBlock)
+    local ppos = getPlayerPosBlockVec()
+    local initialDistance = goalBlock:distance(ppos)
+    if initialDistance < 5 then return end
+    while true do
+      ppos = getPlayerPosBlockVec()
+      local distance = goalBlock:distance(ppos)
+      if initialDistance - distance > initialDistance / 2 then
+        break
+      end
+      local path = gens.getPositionsInDirection(ppos,
+        getDirectionVector(goalBlock), math.floor(distance))
+      if not isSafePath(path, restricted) then
+        break
+      end
+      color = "green"
+      rotateTowards(goalBlock)
+      show(path)
+      coroutine.yield()
+      rb.sShow({ clear = true })
+    end
+  end
+
+
+
+  local function forbiddenRoutine()
+    while true do
+      local goalBlock = randomGoal(inside)
+      local ppos = getPlayerPosBlockVec()
+      local path = gens.getPositionsInDirection(ppos,
+        getDirectionVector(goalBlock), math.floor(goalBlock:distance(ppos)))
+
+      while isSafePath(path, {}) do
+        ppos = getPlayerPosBlockVec()
+        path = gens.getPositionsInDirection(ppos,
+          getDirectionVector(goalBlock), math.floor(goalBlock:distance(ppos)))
+        color = "green"
+        show(path)
+        rotateTowards(goalBlock)
+        rb.sShow({ clear = true })
+        if not restricted[ppos:__tostring()] then
+          return
+        end
+        coroutine.yield()
+      end
+      coroutine.yield()
+    end
+  end
+
+  while true do
+    rb.sShow({ clear = true })
+    local ppos = getPlayerPosBlockVec()
+    local goalBlock = randomGoal(inside)
+    if restricted[ppos:__tostring()] then
+      forbiddenRoutine()
+    else
+      normalRoutine(goalBlock)
+    end
+
+    coroutine.yield()
+  end
+end
+
+
+
+
+local function gensScript(self, args)
+  local gens = MacroCreator:extensions("gens")
+  ---@diagnostic disable
+  ---@type Gens
+  if not gens then return end
+  ---@diagnostic enable
+  _G.GensConfig = _G.GensConfig or {}
+  _G.GensConfig.farmland = args.farmland or "Farmland"
+  _G.GensConfig.crops = args.crops or {
+    ["Wheat Crops"] = true,
+    ["Air"] = true
+  }
+  _G.GensConfig.pitch = args.pitch or 18
+
+  init()
+end
+return { cb = gensScript, options = { saveState = false } }
