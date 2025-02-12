b969bc977602f940f969af351fbc638167e4c1a0
k4ey <kaeyel12@gmail.com>
2025-02-12 17:53:48 +0100

    whoopsie forgot to push

---
+++ macros/goForwardNoCollision.lua
@@ -0,0 +1,28 @@
+local function asyncSleepClock(ms)
+  local now = os.clock()
+  local future = now + ms / 1000
+  repeat
+    coroutine.yield()
+  until os.clock() >= future
+end
+--- assumes player is looking at  0, -90
+local function goForward(self, args)
+  local time = args and args.customTime or 200
+  if args and args.sprint then
+    if not playerDetails.isSneaking() and not playerDetails.isSprinting() then
+      sprint(true)
+    end
+  end
+  if time == -1 then
+    forward(-1)
+    return
+  end
+  if not playerDetails.isCollidedHorz() then
+    sprint(true)
+    forward(time)
+  end
+  asyncSleepClock(200)
+end
+
+--- add no reset to once types
+return { cb = goForward, options = { saveState = true } }
